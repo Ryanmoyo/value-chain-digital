aff99a2690bad53b7776f72deee53c7010f3a276
Ryanmoyo <ryanmoyodriver@gmail.com>
2026-01-26 21:30:24 +0200

    fixed image

--- src/components/home/BannerSection.tsx
+++ src/components/home/BannerSection.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import digitizedCityBanner from "src/assets/digitized_city_banner2.jpeg";
+import digitizedCityBanner from "@/assets/digitized_city_banner2.jpeg";
 
 const BannerSection = () => {
   return (
